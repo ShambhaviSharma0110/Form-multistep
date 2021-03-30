76c0732c3f6d4b27b3f4d2235b6d26e1e97f7d75
shambhavisharma0110 <shambelieber@gmail.com>
2021-03-30 22:19:01 +0530

    react-setup

---
+++ form/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import User from './Components/User';
+
+
+class App extends Component{
+  render(){
+  return (
+    <div className="App">
+      <User/>
+      </div>
+  );
+ }
+}
+
+export default App;
